524d220f164638a61f95511e934095c1e73bec24
Andriy Makarov <angeltek69@gmail.com>
2022-11-27 21:22:15 +0100

    add currentuser

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -23,6 +23,12 @@ router.post(
 
 router.post("/login", validateBody(loginSchema), ctrlWrapper(ctrl.login));
 
+router.post(
+  "/getcurrentuser",
+  // validateBody(refreshSchema),
+  ctrlWrapper(ctrl.getCurrentUser)
+);
+
 router.get("/logout", authenticate, ctrlWrapper(ctrl.logout));
 
 router.post("/refresh", validateBody(refreshSchema), ctrlWrapper(ctrl.refresh));
