7b940acf72c5e7a024f142fdf593969f4a4948b7
Andriy Makarov <angeltek69@gmail.com>
2022-12-05 02:07:14 +0100

    google/facebook fix

--- middlewares/strategies/googleStrategy.js
+++ middlewares/strategies/googleStrategy.js
@@ -3,13 +3,15 @@ const { User } = require("../../models");
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcryptjs");
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL } =
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL, APP_URL } =
   process.env;
+
+const callbackURL = `${APP_URL}${GOOGLE_CALLBACK_URL}`;
 
 const googleParams = {
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: GOOGLE_CALLBACK_URL,
+  callbackURL,
 };
 
 const googleCallback = async (
